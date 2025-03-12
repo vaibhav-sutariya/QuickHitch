b1148649078d85ef77bb23b3a5ba44152f1a7a27
vaibhav-sutariya <vsutariya428@gmail.com>
2025-03-12 11:14:44 +0530

    Add PlaceEnterField widget for location input and update FindRideScreen layout

--- lib/view/home/find/find_ride_screen.dart
+++ lib/view/home/find/find_ride_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quick_hitch/configs/colors/app_colors.dart';
 import 'package:quick_hitch/configs/components/custom_app_bar.dart';
+import 'package:quick_hitch/configs/components/custom_divider.dart';
+import 'package:quick_hitch/view/home/widgets/place_enter_field.dart';
 
 class FindRideScreen extends StatelessWidget {
   const FindRideScreen({super.key});
@@ -11,6 +14,33 @@ class FindRideScreen extends StatelessWidget {
         title: 'Find Ride',
         isLeading: true,
         isAction: false,
+      ),
+      body: Column(
+        children: [
+          CustomDivider(),
+          const SizedBox(height: 20),
+          Column(
+            children: [
+              PlaceEnterField(
+                iconColor: AppColors.greenColor2,
+                hintText: 'Enter your location',
+                onLocationSelected: (location, lat, lng) {
+                  // viewModel.setDepartureLocation(location, lat, lng);
+                  // viewModel.setStopLocation(location, lat, lng);
+                },
+              ),
+              const SizedBox(height: 10),
+              PlaceEnterField(
+                iconColor: AppColors.redColor,
+                hintText: 'Enter destination location',
+                onLocationSelected: (location, lat, lng) {
+                  // viewModel.setDestinationLocation(location, lat, lng);
+                  // viewModel.setStopLocation(location, lat, lng);
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
