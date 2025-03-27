42bfcf515dfc13de5e3d9e8dcde5d0d4b7ff87b8
vaibhav-sutariya <vsutariya428@gmail.com>
2025-03-27 14:19:14 +0530

    Refactor SmallButtonWidget to make isLoading a non-nullable parameter with a default value; update loading state handling in the UI.

--- lib/configs/components/small_button_widget.dart
+++ lib/configs/components/small_button_widget.dart
@@ -4,21 +4,21 @@ import 'package:quick_hitch/configs/colors/app_colors.dart';
 class SmallButtonWidget extends StatelessWidget {
   final Color color;
   final Function() onPressed;
-  final bool? isLoading;
+  final bool isLoading;
   final String text;
   final IconData icon;
   const SmallButtonWidget({
     super.key,
     required this.color,
     required this.onPressed,
-    this.isLoading,
+    this.isLoading = false,
     required this.text,
     required this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
-    return isLoading!
+    return isLoading
         ? CircularProgressIndicator()
         : ElevatedButton.icon(
             style: ElevatedButton.styleFrom(
